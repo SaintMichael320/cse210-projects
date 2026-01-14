526c2b86d6c68564cc5f6268d1e3fdac2d7d49f8
SaintMichael320 <pienaarmichael26@gmail.com>
2026-01-14 08:58:07 +0200

    Activity on Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,66 @@ using System;
 
 class Program
 {
+    // Create ONE Random object for the whole program
+    static Random random = new Random();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        // Main game loop
+        while (playAgain.ToLower() == "yes")
+        {
+            PlayGame();
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine().Trim().ToLower();
+        }
+
+        Console.WriteLine("Thanks for playing!");
+    }
+
+    static void PlayGame()
+    {
+        // Generate random number between 1 and 100
+        int magicNumber = random.Next(1, 101);
+
+        int guess = 0;
+        int numberOfGuesses = 0;
+
+        // Loop until guessed correctly
+        while (guess != magicNumber)
+        {
+            // Get valid user input
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                if (int.TryParse(Console.ReadLine(), out guess))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Please enter a valid number.");
+            }
+
+            numberOfGuesses++;
+
+            // Give feedback
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        Console.WriteLine($"It took you {numberOfGuesses} guesses.");
+        Console.WriteLine();
     }
 }
