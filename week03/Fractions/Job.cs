e956d1ab62dfdc28be9f40b82c95b257ddf8d3b9
SaintMichael320 <pienaarmichael26@gmail.com>
2026-01-24 08:19:40 +0200

    Week3

---
+++ week03/Fractions/Job.cs
@@ -0,0 +1,18 @@
+public class Job
+{
+    // Member variables (fields)
+    private string _company;
+    private string _jobTitle;
+    private int _startYear;
+    private int _endYear;
+
+    // Constructor
+    public Job()
+    {
+    }
+
+    // Methods
+    public void Display()
+    {
+    }
+}
---
+++ week03/Fractions/Resume.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+public class Resume
+{
+    // Member variables (fields)
+    private string _name;
+    private List<Job> _jobs;
+
+    // Constructor
+    public Resume()
+    {
+        _jobs = new List<Job>();
+    }
+
+    // Methods
+    public void Display()
+    {
+    }
+}
