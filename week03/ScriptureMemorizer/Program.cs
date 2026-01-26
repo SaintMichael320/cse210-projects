448283b1e8ce304ee5fbc5f9812b509defeac2bb
SaintMichael320 <pienaarmichael26@gmail.com>
2026-01-26 18:42:15 +0200

    week03 scripture

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // The program hides 3 random words per round instead of just 1.
+
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+
+        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding " +
+                      "in all thy ways acknowledge him and he shall direct thy paths";
+
+        Scripture scripture = new Scripture(reference, text);
+
+        while (true)
+        {
+            Console.Clear();
+            scripture.Display();
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("\nAll words are hidden. Program ending.");
+                break;
+            }
+
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitWords = text.Split(" ");
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(_reference.GetDisplayText());
+        Console.WriteLine();
+
+        foreach (Word word in _words)
+        {
+            Console.Write(word.GetDisplayText() + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+
+        List<Word> visibleWords = new List<Word>();
+
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        while (hiddenCount < numberToHide && visibleWords.Count > 0)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+            hiddenCount++;
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
